8bb6e3b4d9b627ebe5737a017412e7333125064b
Logan(로건) <52653675+ssong10@users.noreply.github.com>
2025-03-30 20:57:44 +0900

    Time: 46 ms (35.04%) | Memory: 66.7 MB (24.39%) - LeetSync

---
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.ts
@@ -0,0 +1,27 @@
+function equalPairs(grid: number[][]): number {
+    const rows = {}
+    const cols = {}
+    const put = (obj: Record<string,number> ,arr: number[]) => {
+        const key = arr.join('.')
+        if (key in obj) {
+            obj[key] += 1
+        } else {
+            obj[key] = 1
+        }
+    }
+    for (let i = 0; i<grid.length; i++) {
+        put(rows, grid[i])
+
+        const tmp = grid.map(row => row[i])
+
+        put(cols, tmp)
+    }
+    console.log(rows,cols)
+    let answer = 0;
+    for (let i of Object.keys(rows)) {
+        if (i in cols) {
+            answer += rows[i] * cols[i]
+        }
+    }
+    return answer
+};
