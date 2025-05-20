be0590d85c1311abb6616c2fec394ffc0af90f57
Logan(로건) <52653675+ssong10@users.noreply.github.com>
2025-05-21 01:06:55 +0900

    Time: 58 ms (53.74%) | Memory: 73.8 MB (16.33%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.ts
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.ts
@@ -1,33 +1,76 @@
 class MinHeap {
-    public q: number[]
-    public min: number 
-    constructor() {
-        this.q = []
-    }
+  private heap: number[];
 
-    enqueue(val: number): number {
-        const index = this.q.findIndex(num => num <= val);
-        if (index < 0) {
-            this.q.push(val)
-        } else {
-            this.q.splice(index, 0, val);
+  constructor() {
+    this.heap = [];
+  }
+
+  private swap(i: number, j: number): void {
+    [this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]];
+  }
+
+  enqueue(val: number): void {
+    this.heap.push(val);
+    let current = this.heap.length - 1;
+
+    while (current > 0) {
+      const parent = Math.floor((current - 1) / 2);
+
+      if (this.heap[parent] <= this.heap[current]) {
+        break;
+      }
+
+      this.swap(parent, current);
+      current = parent;
+    }
+  }
+
+  pop(): number {
+    const min = this.heap[0];
+    const last = this.heap.pop();
+
+    if (this.heap.length > 0) {
+      this.heap[0] = last;
+      let current = 0;
+
+      while (true) {
+        const left = 2 * current + 1;
+        const right = 2 * current + 2;
+        let smallest = current;
+
+        if (left < this.heap.length && this.heap[left] < this.heap[smallest]) {
+          smallest = left;
         }
-        return this.q.length
+
+        if (right < this.heap.length && this.heap[right] < this.heap[smallest]) {
+          smallest = right;
+        }
+
+        if (smallest === current) {
+          break;
+        }
+
+        this.swap(current, smallest);
+        current = smallest;
+      }
     }
 
-    pop() {
-        return this.q.pop()
-    }
+    return min;
+  }
+
+  size(): number {
+    return this.heap.length;
+  }
 }
 
 function findKthLargest(nums: number[], k: number): number {
     const minHeap = new MinHeap();
     for (let num of nums) {
-        const size = minHeap.enqueue(num)
-        if (size > k) {
+        minHeap.enqueue(num)
+        if (minHeap.size() > k) {
             minHeap.pop()
         }
     }
 
-    return minHeap.q.pop()
+    return minHeap.pop()
 };
