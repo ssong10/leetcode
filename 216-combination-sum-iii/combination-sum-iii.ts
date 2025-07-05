c327b3bdec7e95a9a94256778450dcbaa3e3dfec
Logan(로건) <52653675+ssong10@users.noreply.github.com>
2025-07-05 11:52:19 +0900

    Time: 3 ms (17.76%) | Memory: 58.2 MB (8.88%) - LeetSync

---
+++ 216-combination-sum-iii/combination-sum-iii.ts
@@ -0,0 +1,26 @@
+function combinationSum3(k: number, n: number): number[][] {
+    const result = []
+
+    const ans = [] 
+    function backtracking(cur: number[], i: number, sum: number) {
+        if (i > 10 || sum > n) {
+            return
+        }
+        if (cur.length > 7) {
+            console.log(cur,i)
+        }
+        if (cur.length === k) {
+            if (sum === n) {
+                result.push(cur)
+            }
+            return
+        }
+        for (let next = i; next < 10; next++) {
+            cur.push(next)
+            backtracking([...cur], next + 1, sum + next)
+            cur.pop()
+        }
+    }
+    backtracking([], 1, 0)
+    return result
+};
