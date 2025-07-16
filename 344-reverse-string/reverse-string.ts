5f06deb2f2bd1f1a258155c6eb0dc50b46b0f34f
Logan(로건) <52653675+ssong10@users.noreply.github.com>
2025-07-16 21:43:55 +0900

    Time: 0 ms (100.00%) | Memory: 61.7 MB (80.30%) - LeetSync

---
+++ 344-reverse-string/reverse-string.ts
@@ -0,0 +1,10 @@
+/**
+ Do not return anything, modify s in-place instead.
+ */
+function reverseString(s: string[]): void {
+    for (let i =0;i<s.length/2;i++) {
+        const tmp = s[i];
+        s[i] = s[s.length - 1- i]
+        s[s.length-1-i] = tmp
+    }
+};
