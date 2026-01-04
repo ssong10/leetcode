a7942a15287bde1da3063ed8128c71681ea1fa7d
Logan(로건) <52653675+ssong10@users.noreply.github.com>
2026-01-04 16:06:27 +0900

    Time: 3 ms (6.48%) | Memory: 58 MB (6.58%) - LeetSync

---
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.ts
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {
+    let cur = head
+    const tmp = []
+    while (cur) {
+        tmp.push(cur)
+        cur = cur.next
+    }
+
+    const target = tmp.length - n
+    console.log(target , target + 1)
+
+    if (target) {
+        tmp[target - 1].next = tmp[target + 1] ?? null
+    } else {
+        return head.next
+    }
+
+    return head
+};
